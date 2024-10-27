5abdf3235a45525eab0321ad5e93fbf153b7a70e
Laurent <coloma.laurent@gmail.com>
2024-10-27 18:02:26 +0100

    fix (config): solve issue with files not loading after build

--- menuiserie-de-margeride/next.config.js
+++ menuiserie-de-margeride/next.config.js
@@ -3,7 +3,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	/* config options here */
-	basePath: "/menuiserie-de-margeride",
+	basePath: "",
 	output: "export",
 	reactStrictMode: true,
 };
