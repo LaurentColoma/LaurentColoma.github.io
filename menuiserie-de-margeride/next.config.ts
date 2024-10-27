9f0a0558f03338dea54d42eda1c1b10a8a295a38
Laurent <coloma.laurent@gmail.com>
2024-10-27 14:16:35 +0100

    Initial commit

---
+++ menuiserie-de-margeride/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	/* config options here */
+	basePath: "/menuiserie-de-margeride",
+	output: "export",
+	reactStrictMode: true,
+};
+
+export default nextConfig;
