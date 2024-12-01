8809c9b1b71190ee86aaea456ef9425aaa4eff87
Laurent <coloma.laurent@gmail.com>
2024-12-01 15:28:10 +0100

    update all images

---
+++ menuiseries-de-margeride/public/assets/_index.tsx
@@ -0,0 +1,161 @@
+import autre_1 from "./autre_1.jpg";
+import autre_2 from "./autre_2.jpg";
+import autre_3 from "./autre_3.jpg";
+import autre_4 from "./autre_4.jpg";
+import autre_5 from "./autre_5.jpg";
+import autre_6 from "./autre_6.jpg";
+import autre_7 from "./autre_7.jpg";
+import autre_8 from "./autre_8.jpg";
+import autre_9 from "./autre_9.jpg";
+import autre_10 from "./autre_10.jpg";
+import autre_11 from "./autre_11.jpg";
+import autre_12 from "./autre_12.jpg";
+import autre_13 from "./autre_13.jpg";
+import autre_14 from "./autre_14.jpg";
+import autre_15 from "./autre_15.jpg";
+import autre_16 from "./autre_16.jpg";
+import autre_17 from "./autre_17.jpg";
+import autre_18 from "./autre_18.jpg";
+import autre_19 from "./autre_19.jpg";
+import autre_20 from "./autre_20.jpg";
+import autre_21 from "./autre_21.jpg";
+
+import barriere_piscine_1 from "./barriere_piscine_1.jpg";
+import barriere_piscine_2 from "./barriere_piscine_2.jpg";
+
+import camion_contact from "./camion-contact.jpg";
+
+import cloture_1 from "./cloture_1.jpg";
+import cloture_2 from "./cloture_2.png";
+import cloture_3 from "./cloture_3.png";
+import cloture_4 from "./cloture_4.jpg";
+import cloture_5 from "./cloture_5.jpg";
+import cloture_6 from "./cloture_6.png";
+
+import diapo_desktop_1 from "./diapo_desktop_1.jpg";
+import diapo_desktop_2 from "./diapo_desktop_2.jpg";
+import diapo_desktop_3 from "./diapo_desktop_3.png";
+
+import diapo_mobile_1 from "./diapo_mobile_1.jpg";
+import diapo_mobile_2 from "./diapo_mobile_2.jpg";
+import diapo_mobile_3 from "./diapo_mobile_3.jpg";
+
+import garde_corps_1 from "./garde_corps_1.png";
+import garde_corps_2 from "./garde_corps_2.png";
+import garde_corps_3 from "./garde_corps_3.jpg";
+import garde_corps_4 from "./garde_corps_4.png";
+import garde_corps_5 from "./garde_corps_5.png";
+import garde_corps_6 from "./garde_corps_6.jpg";
+
+import logo from "./logo.png";
+
+import moustiquaire_1 from "./moustiquaire_1.jpg";
+import moustiquaire_2 from "./moustiquaire_2.jpg";
+import moustiquaire_3 from "./moustiquaire_3.jpg";
+
+import pergolas_1 from "./pergolas_1.jpg";
+import pergolas_2 from "./pergolas_2.jpg";
+import pergolas_3 from "./pergolas_3.jpg";
+import pergolas_4 from "./pergolas_4.png";
+
+import portails_1 from "./portails_1.png";
+import portails_2 from "./portails_2.png";
+import portails_3 from "./portails_3.png";
+import portails_4 from "./portails_4.jpg";
+import portails_5 from "./portails_5.jpg";
+import portails_6 from "./portails_6.jpg";
+import portails_7 from "./portails_7.jpg";
+import portails_8 from "./portails_8.jpg";
+
+import porte_1 from "./porte_1.jpg";
+import porte_2 from "./porte_2.jpg";
+import porte_3 from "./porte_3.jpg";
+import porte_4 from "./porte_4.jpg";
+import porte_5 from "./porte_5.jpg";
+
+import porte_garage_1 from "./porte_garage_1.jpg";
+import porte_garage_2 from "./porte_garage_2.jpg";
+import porte_garage_3 from "./porte_garage_3.jpg";
+import porte_garage_4 from "./porte_garage_4.jpg";
+import porte_garage_5 from "./porte_garage_5.jpg";
+import porte_garage_6 from "./porte_garage_6.jpg";
+import porte_garage_7 from "./porte_garage_7.jpg";
+import porte_garage_8 from "./porte_garage_8.jpg";
+import porte_garage_9 from "./porte_garage_9.jpg";
+import porte_garage_10 from "./porte_garage_10.jpg";
+
+export {
+	autre_1,
+	autre_2,
+	autre_3,
+	autre_4,
+	autre_5,
+	autre_6,
+	autre_7,
+	autre_8,
+	autre_9,
+	autre_10,
+	autre_11,
+	autre_12,
+	autre_13,
+	autre_14,
+	autre_15,
+	autre_16,
+	autre_17,
+	autre_18,
+	autre_19,
+	autre_20,
+	autre_21,
+	barriere_piscine_1,
+	barriere_piscine_2,
+	camion_contact,
+	cloture_1,
+	cloture_2,
+	cloture_3,
+	cloture_4,
+	cloture_5,
+	cloture_6,
+	diapo_desktop_1,
+	diapo_desktop_2,
+	diapo_desktop_3,
+	diapo_mobile_1,
+	diapo_mobile_2,
+	diapo_mobile_3,
+	garde_corps_1,
+	garde_corps_2,
+	garde_corps_3,
+	garde_corps_4,
+	garde_corps_5,
+	garde_corps_6,
+	logo,
+	moustiquaire_1,
+	moustiquaire_2,
+	moustiquaire_3,
+	pergolas_1,
+	pergolas_2,
+	pergolas_3,
+	pergolas_4,
+	portails_1,
+	portails_2,
+	portails_3,
+	portails_4,
+	portails_5,
+	portails_6,
+	portails_7,
+	portails_8,
+	porte_1,
+	porte_2,
+	porte_3,
+	porte_4,
+	porte_5,
+	porte_garage_1,
+	porte_garage_2,
+	porte_garage_3,
+	porte_garage_4,
+	porte_garage_5,
+	porte_garage_6,
+	porte_garage_7,
+	porte_garage_8,
+	porte_garage_9,
+	porte_garage_10,
+};
